d25dfad0a16b8de6e1aea684775b690473ba52bc
yukiyamamuro <40769397+yukiyamamuro@users.noreply.github.com>
2021-11-27 10:49:52 +0900

    make login page

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,21 @@
+import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
+
 export const Login: VFC = memo(() => {
-  return <p>Login</p>;
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" fontSize="lg" textAlign="center">
+          ユーザー管理アプリ
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder="ユーザーID" />
+          <PrimaryButton>ログイン</PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
